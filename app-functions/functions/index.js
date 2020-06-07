e32bcc3f74c5e6f5dd1b4ae54f2195f03f4c3969
Anthony Uzan <anthonyu.dev@gmail.com>
2020-06-07 15:30:18 +0200

    account creation system v1

--- app-functions/functions/index.js
+++ app-functions/functions/index.js
@@ -1,3 +1,4 @@
+const firebase = require('firebase')
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 const express = require('express');
@@ -11,56 +12,40 @@ admin.initializeApp({
 
 // admin.initializeApp();
 
+const firebaseConfig = {
+    apiKey: "AIzaSyA7x5_c9EFGgIcK75uolGz_FYlUkFL6s-U",
+    authDomain: "social-media-5cf15.firebaseapp.com",
+    databaseURL: "https://social-media-5cf15.firebaseio.com",
+    projectId: "social-media-5cf15",
+    storageBucket: "social-media-5cf15.appspot.com",
+    messagingSenderId: "715337846775",
+    appId: "1:715337846775:web:87d492f513197016f0e8f2"
+};
+
 const app = express();
 let db = admin.firestore();
 
-// getPosts WITHOUT express
-// exports.getPosts = functions.https.onRequest((request, response) => {
-//     db.collection('posts').get().then((snapshot) => {
-//         let posts = [];
-//         snapshot.forEach((doc) => {
-//             posts.push(doc.data());
-//         });
-//         return response.json(posts)
-//     })
-//     .catch((err) => console.error(err));
-// });
-
-// createPost WITHOUT express
-// exports.createPost = functions.https.onRequest((request, response) => {
-//     if(request.method !== 'POST'){
-//         return response.status(400).json({ error: 'Method not allowed' });
-//     }
-//     const newPost = {
-//         author: request.body.author,
-//         body: request.body.body,
-//         date: admin.firestore.Timestamp.fromDate(new Date())
-//     };
-
-//     db.collection('posts').add(newPost).then((doc) => {
-//         response.json({ message: `document ${doc.id} created successfully` });
-//     })
-//     .catch((err) => {
-//         response.status(500).json({ error: 'error when trying to create new post!' });
-//         console.error(err);
-//     });
-// });
+firebase.initializeApp(firebaseConfig)
 
 // getPosts WITH express
 app.get('/posts', (request, response) => {
-    db.collection('posts').orderBy('date', 'desc').get().then((snapshot) => {
-        let posts = [];
-        snapshot.forEach((doc) => {
-            posts.push({
-                postId: doc.id,
-                author: doc.data().author,
-                body: doc.data().body,
-                date: doc.data().date
+    db
+        .collection('posts')
+        .orderBy('date', 'desc')
+        .get()
+        .then((snapshot) => {
+            let posts = [];
+            snapshot.forEach((doc) => {
+                posts.push({
+                    postId: doc.id,
+                    author: doc.data().author,
+                    body: doc.data().body,
+                    date: doc.data().date
+                });
             });
-        });
-        return response.json(posts)
-    })
-    .catch((err) => console.error(err));
+            return response.json(posts)
+        })
+        .catch((err) => console.error(err));
 });
 
 // createPost WITH express
@@ -71,14 +56,40 @@ app.post('/post', (request, response) => {
         date: new Date().toISOString()
     };
 
-    db.collection('posts').add(newPost).then((doc) => {
-        response.json({ message: `document ${doc.id} created successfully` });
-    })
-    .catch((err) => {
-        response.status(500).json({ error: 'error when trying to create new post!' });
-        console.error(err);
-    });
+    db
+        .collection('posts')
+        .add(newPost)
+        .then((doc) => {
+            response.json({ message: `document ${doc.id} created successfully` });
+        })
+        .catch((err) => {
+            response.status(500).json({ error: 'error when trying to create new post!' });
+            console.error(err);
+        });
 });
 
-// endpoint in Firebase Functions
+// Signup route
+app.post('/signup', (request, response) => {
+    const newUser = {
+        email: request.body.email,
+        password: request.body.password,
+        confirmPassword: request.body.confirmPassword,
+        username: request.body.username
+    };
+
+    // TODO: validate data
+
+    firebase
+        .auth()
+        .createUserWithEmailAndPassword(newUser.email, newUser.password)
+        .then(data => {
+            return response.status(201).json({ message: `user ${data.user.uid} signed up successfully` });
+        })
+        .catch(err => {
+            console.error(err);
+            return response.status(500).json({ error: err.code })
+        });
+});
+
+// Function to deploy in Firebase Functions
 exports.api = functions.region('europe-west1').https.onRequest(app);
