2d1bd30b4539625c802a1e97160957026d44885c
Anthony Uzan <anthonyu.dev@gmail.com>
2020-06-14 18:57:23 +0200

    Improved post creation process (authentication required)

--- app-functions/functions/index.js
+++ app-functions/functions/index.js
@@ -49,9 +49,43 @@ app.get('/posts', (request, response) => {
         .catch((err) => console.error(err));
 });
 
-app.post('/post', (request, response) => {
+const FBAuth = (request, response, next) => {
+    let idToken;
+    if (request.headers.authorization && request.headers.authorization.startsWith('Bearer ')) {
+        idToken = request.headers.authorization.split('Bearer ')[1];
+    } else {
+        console.error('No token found')
+        return response.status(403).json({ error: 'Unauthorized' })
+    }
+
+    admin
+        .auth()
+        .verifyIdToken(idToken)
+        .then((decodedToken) => {
+            request.user = decodedToken;
+            return db
+                .collection('users')
+                .where('userId', '==', request.user.uid)
+                .limit(1)
+                .get();
+        })
+        .then((data) => {
+            request.user.username = data.docs[0].data().username;
+            return next();
+        })
+        .catch((err) => {
+            console.error('Error while verifying token ', err)
+            return response.status(403).json(err)
+        })
+};
+
+app.post('/post', FBAuth, (request, response) => {
+    if (request.body.body.trim() === '') {
+        return response.status(400).json({ body: 'Must not be empty' })
+    }
+
     const newPost = {
-        author: request.body.author,
+        author: request.user.username,
         body: request.body.body,
         date: new Date().toISOString()
     };
