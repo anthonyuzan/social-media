23f2923447d07be50e30bca21f2821b3f5294404
Anthony Uzan <anthonyu.dev@gmail.com>
2020-06-25 21:58:50 +0200

    Like posts v1

--- app-functions/functions/users/posts.js
+++ app-functions/functions/users/posts.js
@@ -102,3 +102,44 @@ exports.commentOnPost = (request, response) => {
             response.status(500).json({ error: 'Something went wrong' });
         })
 }
+
+exports.likePost = (request, response) => {
+    const likeDocument = db.collection('likes').where('username', '==', request.user.username)
+        .where('postId', '==', request.params.postId).limit(1);
+
+    const postDocument = db.doc(`/posts/${request.params.postId}`);
+
+    let postData = {};
+
+    postDocument.get()
+        .then((doc) => {
+            if (doc.exists) {
+                postData = doc.data();
+                postData.postId = doc.id;
+                return likeDocument.get();
+            } else {
+                return response.status(404).json({ error: 'Post not found' });
+            }
+        })
+        .then((data) => {
+            if (data.empty) {
+                return db.collection('likes').add({
+                    postId: request.params.postId,
+                    username: request.user.username
+                })
+                    .then(() => {
+                        postData.likeCount++;
+                        return postDocument.update({ likeCount: postData.likeCount })
+                    })
+                    .then(() => {
+                        return response.json(postData)
+                    })
+            } else {
+                return response.status(400).json({ error: 'Post already liked' })
+            }
+        })
+        .catch((err) => {
+            console.error(err);
+            response.status(500).json({ error: err.code });
+        })
+}
