4fd4d0ef51467e0dda4f3b80c3347f7202c64519
Anthony Uzan <anthonyu.dev@gmail.com>
2020-07-08 22:36:30 +0200

    Main routes added to App.js

--- app-client/src/App.js
+++ app-client/src/App.js
@@ -1,12 +1,27 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Social media app</h1>
-    </div>
-  );
+// Pages
+import home from './pages/home';
+import login from './pages/login';
+import signup from './pages/signup';
+
+export class App extends Component {
+  render() {
+    return (
+      <div>
+        <Router>
+          <Switch>
+            <Route exact path="/" component={home}/>
+            <Route exact path="/login" component={login}/>
+            <Route exact path="/signup" component={signup}/>
+          </Switch>
+        </Router>
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
+
