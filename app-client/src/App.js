563ef48f4845608bed3f2b7e527c51c1f709513d
Anthony Uzan <anthonyu.dev@gmail.com>
2020-09-08 20:50:19 +0200

    Display posts on the home page

--- app-client/src/App.js
+++ app-client/src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
 import MuiThemeProvider from '@material-ui/core/styles/MuiThemeProvider';
-import createTheme from '@material-ui/core/styles/createMuiTheme';
+import createMuiTheme from '@material-ui/core/styles/createMuiTheme';
 
 // Components
 import Navbar from './components/Navbar';
@@ -12,7 +12,7 @@ import home from './pages/home';
 import login from './pages/login';
 import signup from './pages/signup';
 
-const theme = createTheme({
+const theme = createMuiTheme({
   palette: {
     primary: {
       light: '#33c9dc',
--- app-client/src/components/Navbar.js
+++ app-client/src/components/Navbar.js
@@ -8,17 +8,17 @@ import Button from '@material-ui/core/Button';
 
 
 export class Navbar extends Component {
-    render() {
-        return (
-           <AppBar>
-               <Toolbar className="nav-container">
-                   <Button color="inherit" component={Link} to="/">Home</Button>
-                   <Button color="inherit" component={Link} to="/login">Login</Button> 
-                   <Button color="inherit" component={Link} to="/signup">Signup</Button> 
-               </Toolbar>
-           </AppBar>
-        )
-    }
+  render() {
+    return (
+      <AppBar>
+        <Toolbar className="nav-container">
+          <Button color="inherit" component={Link} to="/">Home</Button>
+          <Button color="inherit" component={Link} to="/login">Login</Button>
+          <Button color="inherit" component={Link} to="/signup">Signup</Button>
+        </Toolbar>
+      </AppBar>
+    )
+  }
 }
 
 export default Navbar
