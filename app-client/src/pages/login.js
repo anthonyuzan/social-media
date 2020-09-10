6b1e6249af4243b839c95d012c69c41840b13a21
Anthony Uzan <anthonyu.dev@gmail.com>
2020-09-10 21:08:28 +0200

    Login page init

--- app-client/src/pages/login.js
+++ app-client/src/pages/login.js
@@ -1,13 +1,42 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import withStyles from '@material-ui/core/styles/withStyles';
+import PropTypes from 'prop-types';
+import AppIcon from '../images/icon.png'
+
+// MUI Stuff
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
+
+const styles = {
+  form: {
+    textAlign: 'center'
+  },
+  image: {
+    margin: '20px auto 20px auto'
+  }
+
+}
 
 export class login extends Component {
   render() {
+    const { classes } = this.props;
     return (
-      <div>
-        <h1>Login Page</h1>
-      </div>
+      <Grid container className={classes.form}>
+        <Grid item sm/>
+        <Grid item sm>
+          <img src={AppIcon} alt="monkey" className={classes.image}/>
+          <Typography variant="h2" className={classes.pageTitle}>
+            Login
+          </Typography>
+        </Grid>
+        <Grid item sm/>
+      </Grid>
     )
   }
 }
 
-export default login
+login.propTypes = {
+  classes: PropTypes.object.isRequired
+}
+
+export default withStyles(styles)(login);
