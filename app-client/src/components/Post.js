c456254655ea777f0a322ffdadcd29b6e6025641
Anthony Uzan <anthonyu.dev@gmail.com>
2020-09-10 19:46:49 +0200

    Improved date display format

--- app-client/src/components/Post.js
+++ app-client/src/components/Post.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import withStyles from '@material-ui/core/styles/withStyles';
 import Typography from '@material-ui/core/Typography';
 import { Link } from 'react-router-dom';
+import dayjs from 'dayjs';
+import relativeTime from 'dayjs/plugin/relativeTime';
 
 // MUI Stuff
 import Card from '@material-ui/core/Card';
@@ -26,13 +28,16 @@ const styles = {
 
 export class Post extends Component {
   render() {
+    dayjs.extend(relativeTime);
     const { classes, post : { body, date, userImage, author, postId, likeCount, commentCount } } = this.props;
     return (
       <Card className={classes.card}>
         <CardMedia image={userImage} title="Profile image" className={classes.image}/>
         <CardContent className={classes.content}>
           <Typography variant="h5" component={Link} to={`/users/${author}`} color="primary">{author}</Typography>
-          <Typography variant="body2" color="textSecondary">{date}</Typography>
+          <Typography variant="body2" color="textSecondary">
+            {dayjs(date).fromNow()}
+          </Typography>
           <Typography variant="body1">{body}</Typography>
         </CardContent>
       </Card>
