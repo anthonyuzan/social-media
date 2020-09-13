b7f9adc0a65b2586642e7daafe40c8391ff835a2
Anthony Uzan <anthonyu.dev@gmail.com>
2020-09-13 21:04:27 +0200

    Login details added

--- app-client/src/pages/login.js
+++ app-client/src/pages/login.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import withStyles from '@material-ui/core/styles/withStyles';
 import PropTypes from 'prop-types';
 import AppIcon from '../images/icon.png'
+import axios from 'axios'
 
 // MUI Stuff
 import Grid from '@material-ui/core/Grid';
@@ -39,7 +40,29 @@ export class login extends Component {
     }
   }
   handleSubmit = (event) => {
-    console.log('hi')
+    event.preventDefault();
+    this.setState({
+      loading: true
+    });
+    const userData = {
+      email: this.state.email,
+      password: this.state.password
+    }
+    axios
+      .post('/login', userData)
+      .then((res) => {
+        console.log(res.data);
+        this.setState({
+          loading: false
+        })
+        this.props.history.push('/');
+      })
+      .catch((err) => {
+        this.setState({
+          errors: err.response.data,
+          loading: false
+        })
+      })
   }
   handleChange = (event) => {
     this.setState({
@@ -48,6 +71,7 @@ export class login extends Component {
   }
   render() {
     const { classes } = this.props;
+    const { errors, loading } = this.state;
     return (
       <Grid container className={classes.form}>
         <Grid item sm />
@@ -57,8 +81,8 @@ export class login extends Component {
             Login
           </Typography>
           <form noValidate onSubmit={this.handleSubmit}>
-            <TextField id="email" name="email" type="email" label="Email" className={classes.textField} value={this.state.email} onChange={this.handleChange} fullWidth />
-            <TextField id="password" name="password" type="password" label="Password" className={classes.textField} value={this.state.password} onChange={this.handleChange} fullWidth />
+            <TextField id="email" name="email" type="email" label="Email" className={classes.textField} helperText={errors.email} error={errors.email ? true : false} value={this.state.email} onChange={this.handleChange} fullWidth />
+            <TextField id="password" name="password" type="password" label="Password" className={classes.textField} helperText={errors.password} error={errors.password ? true : false} value={this.state.password} onChange={this.handleChange} fullWidth />
             <Button type="submit" variant="contained" color="primary" className={classes.button}>
               Login
             </Button>
