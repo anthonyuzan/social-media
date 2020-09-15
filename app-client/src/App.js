3629e71f25c4eb84e0cfbebf6b705c155df5609d
Anthony Uzan <anthonyu.dev@gmail.com>
2020-09-15 11:52:55 +0200

    Theme file created

--- app-client/src/App.js
+++ app-client/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
 import { ThemeProvider as MuiThemeProvider } from '@material-ui/core/styles';
 import createMuiTheme from '@material-ui/core/styles/createMuiTheme';
+import themeFile from './util/theme';
 
 // Components
 import Navbar from './components/Navbar';
@@ -12,48 +13,7 @@ import home from './pages/home';
 import login from './pages/login';
 import signup from './pages/signup';
 
-const theme = createMuiTheme({
-  palette: {
-    primary: {
-      light: '#33c9dc',
-      main: '#00bcd4',
-      dark: '#008394',
-      contrastText: '#fff'
-    },
-    secondary: {
-      light: '#ff6333',
-      main: '#ff3d00',
-      dark: '#b22a00',
-      contrastText: '#fff'
-    }
-  },
-  spreadThis: {
-    form: {
-      textAlign: 'center'
-    },
-    image: {
-      margin: '20px auto 20px auto'
-    },
-    pageTitle: {
-      margin: '10px auto 10px auto'
-    },
-    textField: {
-      margin: '10px auto 10px auto'
-    },
-    button: {
-      marginTop: 20,
-      position: 'relative'
-    },
-    customError: {
-      marginTop: 10,
-      color: 'red',
-      fontSize: '0.8rem'
-    },
-    progess: {
-      position: 'absolute'
-    }
-  }
-});
+const theme = createMuiTheme(themeFile);
 
 export class App extends Component {
   render() {
