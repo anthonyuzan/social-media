190567f84b5650221ac7d73863d22338871151d7
Anthony Uzan <anthonyu.dev@gmail.com>
2020-09-16 17:37:43 +0200

    Login process updated with Redux

--- app-client/src/App.js
+++ app-client/src/App.js
@@ -3,8 +3,12 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
 import { ThemeProvider as MuiThemeProvider } from '@material-ui/core/styles';
 import createMuiTheme from '@material-ui/core/styles/createMuiTheme';
-import themeFile from './util/theme';
+import themeObject from './util/theme';
 import jwtDecode from 'jwt-decode';
+
+// Redux
+import { Provider } from 'react-redux';
+import store from './redux/store';
 
 // Components
 import Navbar from './components/Navbar';
@@ -15,15 +19,15 @@ import home from './pages/home';
 import login from './pages/login';
 import signup from './pages/signup';
 
-const theme = createMuiTheme(themeFile);
+const theme = createMuiTheme(themeObject);
 
 let authenticated;
 const token = localStorage.FBIdToken;
 if (token) {
   const decodedToken = jwtDecode(token);
   if (decodedToken.exp * 1000 < Date.now()) {
-    window.location.href = '/login'
     authenticated = false;
+    window.location.href = '/login';
   } else {
     authenticated = true;
   }
@@ -33,22 +37,32 @@ export class App extends Component {
   render() {
     return (
       <MuiThemeProvider theme={theme}>
-        <div className="App">
+        <Provider store={store}>
           <Router>
             <Navbar />
             <div className="container">
               <Switch>
                 <Route exact path="/" component={home} />
-                <AuthRoute exact path="/login" component={login} authenticated={authenticated} />
-                <AuthRoute exact path="/signup" component={signup} authenticated={authenticated} />
+                <AuthRoute
+                  exact
+                  path="/login"
+                  component={login}
+                  authenticated={authenticated}
+                />
+                <AuthRoute
+                  exact
+                  path="/signup"
+                  component={signup}
+                  authenticated={authenticated}
+                />
               </Switch>
             </div>
           </Router>
-        </div>
+        </Provider>
       </MuiThemeProvider>
-    )
+    );
   }
 }
 
-export default App
+export default App;
 
--- app-client/src/pages/home.js
+++ app-client/src/pages/home.js
@@ -7,23 +7,29 @@ import Post from '../components/Post'
 export class home extends Component {
   state = {
     posts: null
-  }
+  };
+
   componentDidMount() {
-    axios.get('/posts')
+    axios
+      .get('/posts')
       .then((res) => {
-        console.log(res.data)
         this.setState({
           posts: res.data
-        })
+        });
       })
-      .catch((err) => {
-        console.log(err)
-      })
+      .catch((err) => console.log(err));
   }
+
   render() {
     let recentPostsMarkup = this.state.posts ? (
-    this.state.posts.map((post) => <Post key={post.postId} post={post}/>)
-    ) : <p>Loading...</p>
+      this.state.posts.map((post) => (
+        <Post key={post.postId} post={post} />
+      ))
+    ) :
+      (
+        <p>Loading...</p>
+      );
+    
     return (
       <Grid container spacing={2}>
         <Grid item sm={8} xs={12}>
@@ -33,9 +39,8 @@ export class home extends Component {
           <p>Profile...</p>
         </Grid>
       </Grid>
-
-    )
+    );
   }
 }
 
-export default home
+export default home;
