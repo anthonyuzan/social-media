3c025cd6359de2bab8f66186fd2291931224b410
Anthony Uzan <anthonyu.dev@gmail.com>
2020-09-20 21:26:05 +0200

    Profile image upload

--- app-client/src/components/Profile.js
+++ app-client/src/components/Profile.js
@@ -9,14 +9,18 @@ import Button from '@material-ui/core/Button';
 import MuiLink from '@material-ui/core/Link';
 import Typography from '@material-ui/core/Typography';
 import Paper from '@material-ui/core/Paper';
+import IconButton from '@material-ui/core/IconButton';
+import Tooltip from '@material-ui/core/Tooltip';
 
 // Redux Stuff
 import { connect } from 'react-redux';
+import { logoutUser, uploadImage } from '../redux/actions/userActions';
 
 // Icons
 import LocationOn from '@material-ui/icons/LocationOn';
 import LinkIcon from '@material-ui/icons/Link';
 import CalendarToday from '@material-ui/icons/CalendarToday';
+import EditIcon from '@material-ui/icons/Edit';
 
 const styles = (theme) => ({
   paper: {
@@ -67,6 +71,20 @@ const styles = (theme) => ({
 });
 
 export class Profile extends Component {
+  userImageChange = (event) => {
+    const image = event.target.files[0];
+    // send to server
+    const formData = new FormData();
+    formData.append('image', image, image.name);
+    this.props.uploadImage(formData);
+
+  }
+
+  userEditPicture = () => {
+    const fileInput = document.getElementById('imageInput');
+    fileInput.click();
+  }
+
   render() {
     const {
       classes,
@@ -81,7 +99,13 @@ export class Profile extends Component {
       <Paper className={classes.paper}>
         <div className={classes.profile}>
           <div className="image-wrapper">
-            <img src={imageUrl} alt="profile" className="profile-image"/>
+            <img src={imageUrl} alt="profile" className="profile-image" />
+            <input type="file" id="imageInput" hidden="hidden" onChange={this.userImageChange} />
+            <Tooltip title="Edit profile picture" placement="top">
+              <IconButton onClick={this.userEditPicture} className="button">
+                <EditIcon color="primary" />
+              </IconButton>
+            </Tooltip>
           </div>
 
           <hr />
@@ -104,7 +128,7 @@ export class Profile extends Component {
                 <hr />
               </Fragment>
             )}
-            
+
             {website && (
               <Fragment>
                 <LinkIcon color="primary" />
@@ -115,27 +139,27 @@ export class Profile extends Component {
               </Fragment>
             )}
 
-            <CalendarToday color="primary"/>{' '}
+            <CalendarToday color="primary" />{' '}
             <span>Joined {dayjs(createdAt).format('MMM YYYY')}</span>
 
           </div>
         </div>
       </Paper>
     ) : (
-      <Paper className={classes.paper}>
-        <Typography variant="body2" align="center">
-          No profile found, please login again
+        <Paper className={classes.paper}>
+          <Typography variant="body2" align="center">
+            No profile found, please login again
         </Typography>
-        <div className={classes.buttons}>
-          <Button variant="contained" color="primary" component={Link} to="/login">
-            Login
+          <div className={classes.buttons}>
+            <Button variant="contained" color="primary" component={Link} to="/login">
+              Login
           </Button>
-          <Button variant="contained" color="secondary" component={Link} to="/signup">
-            Signup
+            <Button variant="contained" color="secondary" component={Link} to="/signup">
+              Signup
           </Button>
-        </div>
-      </Paper>
-    )) : (<p>loading...</p>)
+          </div>
+        </Paper>
+      )) : (<p>loading...</p>)
 
     return profileMarkup;
   }
@@ -145,9 +169,13 @@ const mapStateToProps = (state) => ({
   user: state.user
 });
 
+const mapActionsToProps = { logoutUser, uploadImage };
+
 Profile.propTypes = {
+  logoutUser: PropTypes.func.isRequired,
+  uploadImage: PropTypes.func.isRequired,
   user: PropTypes.object.isRequired,
   classes: PropTypes.object.isRequired
 };
 
-export default connect(mapStateToProps)(withStyles(styles)(Profile))
+export default connect(mapStateToProps, mapActionsToProps)(withStyles(styles)(Profile))
