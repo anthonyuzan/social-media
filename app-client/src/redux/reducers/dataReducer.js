5fb484e9eadd804c5b73ed535f698a2b56abc3ca
Anthony Uzan <anthonyu.dev@gmail.com>
2020-09-26 18:11:48 +0200

    Auto remove post on the front

--- app-client/src/redux/reducers/dataReducer.js
+++ app-client/src/redux/reducers/dataReducer.js
@@ -27,10 +27,9 @@ export default function(state = initialState, action){
         ...state
       }
     case DELETE_POST:
-      index = state.posts.findIndex((post) => post.postId === action.payload);
-      state.posts.splice(index, 1);
       return {
-        ...state
+        ...state,
+        posts: state.posts.filter((post) => post.postId !== action.payload )
       }
     default:
       return state;
