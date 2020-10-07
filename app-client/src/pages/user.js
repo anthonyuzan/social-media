31bc4a1e3e56c444a0dce6250f13cdd9345dca86
Anthony Uzan <anthonyu.dev@gmail.com>
2020-10-07 12:59:34 +0200

    Loading skeletons for profile added

--- app-client/src/pages/user.js
+++ app-client/src/pages/user.js
@@ -4,6 +4,7 @@ import axios from 'axios';
 import Post from '../components/post/Post';
 import StaticProfile from '../components/profile/StaticProfile';
 import PostSkeleton from '../util/PostSkeleton';
+import ProfileSkeleton from '../util/ProfileSkeleton';
 
 // MUI Stuff
 import Grid from '@material-ui/core/Grid';
@@ -58,7 +59,7 @@ class user extends Component {
           </Grid>
           <Grid item sm={4} xs={12}>
             {this.state.profile === null ? (
-              <p>Loading profile...</p>
+              <ProfileSkeleton />
             ) : (
               <StaticProfile profile={this.state.profile} />
             )}
